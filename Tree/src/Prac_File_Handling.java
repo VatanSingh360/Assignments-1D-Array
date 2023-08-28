be7d71534aa4bd74fd3ee53171345e98194b5127
Vatan Singh <vatansingh1996@gmail.com>
2023-08-28 16:36:02 +0530

    File handling

---
+++ Tree/src/Prac_File_Handling.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Prac_File_Handling {
+    public static void main(String[] args) {
+         //create a new file
+//        File myFile = new File("first.txt");
+//        try {
+//            myFile.createNewFile();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        // write in file
+//        try {
+//            FileWriter myFile = new FileWriter("first.txt");
+//            myFile.write("this is my first file\n bye for now . ");
+//            myFile.close();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        // read a file
+//        File myFile = new File("First.txt");
+//        try {
+//            Scanner sc = new Scanner(myFile);
+//            while (sc.hasNextLine()){
+//                String a = sc.nextLine();
+//                System.out.println(a);
+//            }
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+        // Delete a file
+//        File myFile = new File("First.txt");
+//        if (myFile.delete()) System.out.println("File deleted");
+//        else System.out.println("problem occourd ");
+
+    }
+}
